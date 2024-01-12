fbe394f7ebee9c5b15d7fecfa5facfe337ae60aa
BaoNhanTran <tranxuandinh181092@gmail.com>
2024-01-12 14:55:37 +0700

    From Bài 90 to Bài 95

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+// Layouts
+import { HeaderOnly } from '~/components/Layout';
+
+// Pages
+import { Home, Following, Upload, Profile, Search } from '~/pages';
+
+// Public Routes
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
