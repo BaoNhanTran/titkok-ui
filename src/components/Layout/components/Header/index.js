0d56bd9ab1c69e66ebc7b84f0804d4378095b63d
BaoNhanTran <tranxuandinh181092@gmail.com>
2024-01-12 22:17:09 +0700

    Bài 96. Xây dựng UI phần Header #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,9 +1,29 @@
+import images from '~/assets/images';
+import { ClearIcon, LoadingIcon, SearchIcon } from '~/components/Icon';
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 const cx = classNames.bind(styles);
 
 function Header() {
-    return <header className={cx('wrapper')}></header>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('logo')}>
+                <img src={images.logo} alt="Tittok" />
+            </div>
+            <div className={cx('search')}>
+                <input placeholder="Search" />
+                <div className={cx('icon-wrapper')}>
+                    <ClearIcon className={cx('clear-btn')} />
+                    <LoadingIcon className={cx('loading')} />
+                </div>
+                <span className={cx('separate')}></span>
+                <button className={cx('search-btn')}>
+                    <SearchIcon />
+                </button>
+            </div>
+            <div className={cx('actions')}></div>
+        </header>
+    );
 }
 
 export default Header;
