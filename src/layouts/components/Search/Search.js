cf89088b2d4aa1b1c87a1250341227061229f95c
BaoNhanTran <tranxuandinh181092@gmail.com>
2024-03-30 10:14:49 +0700

    Bài 106. Tìm hiểu và sử dụng thư viện Axios

--- src/layouts/components/Search/Search.js
+++ src/layouts/components/Search/Search.js
@@ -4,6 +4,7 @@ import { Wrapper as PopperWrapper } from '~/components/Popper';
 import { CircleXmarkIcon, MagnifyingGlassIcon, SpinnerIcon } from '~/components/Icons/Icons';
 import AccountItem from '~/components/AccountItem';
 import useDebouce from '~/hooks/useDebounce';
+import * as searchService from '~/services/searchService';
 import classNames from 'classnames/bind';
 import styles from './Search.module.scss';
 
@@ -21,20 +22,20 @@ function Search() {
 
     useEffect(() => {
         if (!debouncedValue) {
+            setSearchResult([]);
             return;
         }
 
-        setLoading(true);
+        const fetchApi = async () => {
+            setLoading(true);
 
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debouncedValue)}&type=less`)
-            .then((res) => res.json())
-            .then((res) => {
-                setLoading(false);
-                setSearchResult(res.data);
-            })
-            .catch(() => {
-                setLoading(false);
-            });
+            const res = await searchService.search(debouncedValue);
+            setSearchResult(res);
+
+            setLoading(false);
+        };
+
+        fetchApi();
     }, [debouncedValue]);
 
     const handleInput = (e) => {
@@ -54,41 +55,46 @@ function Search() {
     };
 
     return (
-        <HeadlessTippy
-            visible={debouncedValue && showResult}
-            interactive
-            onClickOutside={handleHideResult}
-            render={(attrs) => (
-                <div className={cx('search-result')} tabIndex="-1" {...attrs}>
-                    <PopperWrapper>
-                        <header className={cx('search-title')}>Accounts</header>
-                        {searchResult.map((result) => (
-                            <AccountItem key={result.id} data={result} />
-                        ))}
-                    </PopperWrapper>
-                </div>
-            )}
-        >
-            <div className={cx('search')}>
-                <input
-                    placeholder="Search"
-                    value={searchValue}
-                    onChange={handleInput}
-                    ref={inputRef}
-                    onFocus={() => setShowResult(true)}
-                />
-                {searchValue && !loading && (
-                    <button className={cx('clear')} onClick={handleClear}>
-                        <CircleXmarkIcon />
-                    </button>
+        // Interactive tippy element may not be accessible via keyboard navigation
+        // because it is not directly after the reference element in the DOM source order.
+        // Using a wrapper <div> tag around the reference element solves this by creating a new parentNode context.
+        <div>
+            <HeadlessTippy
+                visible={searchResult.length > 0 && showResult}
+                interactive
+                onClickOutside={handleHideResult}
+                render={(attrs) => (
+                    <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                        <PopperWrapper>
+                            <header className={cx('search-title')}>Accounts</header>
+                            {searchResult.map((result) => (
+                                <AccountItem key={result.id} data={result} />
+                            ))}
+                        </PopperWrapper>
+                    </div>
                 )}
-                {loading && <SpinnerIcon className={cx('loading')} />}
-                <span className={cx('separate')}></span>
-                <button className={cx('search-btn')}>
-                    <MagnifyingGlassIcon />
-                </button>
-            </div>
-        </HeadlessTippy>
+            >
+                <div className={cx('search')}>
+                    <input
+                        placeholder="Search"
+                        value={searchValue}
+                        onChange={handleInput}
+                        ref={inputRef}
+                        onFocus={() => setShowResult(true)}
+                    />
+                    {searchValue && !loading && (
+                        <button className={cx('clear')} onClick={handleClear}>
+                            <CircleXmarkIcon />
+                        </button>
+                    )}
+                    {loading && <SpinnerIcon className={cx('loading')} />}
+                    <span className={cx('separate')}></span>
+                    <button className={cx('search-btn')}>
+                        <MagnifyingGlassIcon />
+                    </button>
+                </div>
+            </HeadlessTippy>
+        </div>
     );
 }
 
