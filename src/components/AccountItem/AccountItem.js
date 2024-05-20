d1819513cb326b81289d8a30a3b2c6e10faeda07
BaoNhanTran <tranxuandinh181092@gmail.com>
2024-05-20 14:30:51 +0700

    Bài 97. Xây dựng UI phần Header #2

---
+++ src/components/AccountItem/AccountItem.js
@@ -0,0 +1,26 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { CircleCheckIcon } from '~/Icons';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/7a2097efcb5b9944b9ea06c89c838627~c5_300x300.webp?lk3s=a5d48078&nonce=94152&refresh_token=5555e5b2b165d334ee30d9acf73c3121&x-expires=1716357600&x-signature=t7Lw6IvWIv2DpS8qoKI415veD5E%3D&shp=a5d48078&shcp=c1333099"
+                alt="nguyenvana"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('nickname')}>
+                    <p>nguyenvana</p>
+                    <CircleCheckIcon className={cx('check')} />
+                </h4>
+                <p className={cx('name')}>Nguyen Van A</p>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
--- src/layouts/components/Search/Search.js
+++ src/layouts/components/Search/Search.js
@@ -1,19 +1,45 @@
+import HeadlessTippy from '@tippyjs/react/headless';
+import { CircleNotchIcon, CircleXmarkIcon, MagnifyingGlassIcon } from '~/components/Icons';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountItem from '~/components/AccountItem';
 import classNames from 'classnames/bind';
 import styles from './Search.module.scss';
-import { CircleNotchIcon, CircleXmarkIcon, MagnifyingGlassIcon } from '~/Icons';
 
 const cx = classNames.bind(styles);
 
 function Search() {
     return (
-        <div className={cx('search')}>
-            <input placeholder="Search" />
-            <CircleXmarkIcon className={cx('clear-btn')} />
-            <CircleNotchIcon className={cx('loading')} />
-            <span className={cx('separate')}></span>
-            <button className={cx('search-btn')}>
-                <MagnifyingGlassIcon />
-            </button>
+        // Interactive tippy element may not be accessible via keyboard navigation
+        // because it is not directly after the reference element in the DOM source order.
+        // Using a wrapper <div> tag around the reference element solves this by creating a new parentNode context.
+        <div>
+            <HeadlessTippy
+                render={(attrs) => (
+                    <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                        <PopperWrapper>
+                            <h4 className={cx('search-title')}>Accounts</h4>
+                            <AccountItem />
+                            <AccountItem />
+                            <AccountItem />
+                            <AccountItem />
+                        </PopperWrapper>
+                    </div>
+                )}
+                interactive
+                visible
+                placement="bottom"
+                offset={[0, 8]}
+            >
+                <div className={cx('search')}>
+                    <input placeholder="Search" />
+                    <CircleXmarkIcon className={cx('clear-btn')} />
+                    <CircleNotchIcon className={cx('loading')} />
+                    <span className={cx('separate')}></span>
+                    <button className={cx('search-btn')}>
+                        <MagnifyingGlassIcon />
+                    </button>
+                </div>
+            </HeadlessTippy>
         </div>
     );
 }
