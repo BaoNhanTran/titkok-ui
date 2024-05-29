c96fbbe86f173f5617c75aef87aff4a217a518b4
BaoNhanTran <tranxuandinh181092@gmail.com>
2024-05-29 08:27:04 +0700

    Bài 103. Xây dựng Logic phần Header #1

--- src/layouts/components/Search/Search.js
+++ src/layouts/components/Search/Search.js
@@ -1,3 +1,4 @@
+import { useRef, useState, useEffect } from 'react';
 import HeadlessTippy from '@tippyjs/react/headless';
 import { CircleNotchIcon, CircleXmarkIcon, MagnifyingGlassIcon } from '~/components/Icons';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
@@ -8,12 +9,45 @@ import styles from './Search.module.scss';
 const cx = classNames.bind(styles);
 
 function Search() {
+    const [searchResult, setSearchResult] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
+    const [showResult, setShowResult] = useState(false);
+
+    const inputRef = useRef('');
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([1]);
+        }, 0);
+    }, [searchValue]);
+
+    const handleInput = (e) => {
+        const value = e.target.value;
+        if (!value.startsWith(' ')) {
+            setSearchValue(value);
+        }
+    };
+
+    const handleClear = () => {
+        setSearchValue('');
+        inputRef.current.focus();
+    };
+
+    const handleHideResult = () => {
+        setShowResult(false);
+    };
+
     return (
         // Interactive tippy element may not be accessible via keyboard navigation
         // because it is not directly after the reference element in the DOM source order.
         // Using a wrapper <div> tag around the reference element solves this by creating a new parentNode context.
         <div>
             <HeadlessTippy
+                visible={searchResult.length > 0 && searchValue && showResult}
+                interactive
+                placement="bottom"
+                offset={[0, 8]}
+                onClickOutside={handleHideResult}
                 render={(attrs) => (
                     <div className={cx('search-result')} tabIndex="-1" {...attrs}>
                         <PopperWrapper>
@@ -25,14 +59,18 @@ function Search() {
                         </PopperWrapper>
                     </div>
                 )}
-                interactive
-                visible
-                placement="bottom"
-                offset={[0, 8]}
             >
                 <div className={cx('search')}>
-                    <input placeholder="Search" />
-                    <CircleXmarkIcon className={cx('clear-btn')} />
+                    <input
+                        placeholder="Search"
+                        value={searchValue}
+                        onChange={handleInput}
+                        onFocus={() => setShowResult(true)}
+                        ref={inputRef}
+                    />
+                    <button className={cx('clear-btn')} onClick={handleClear}>
+                        <CircleXmarkIcon />
+                    </button>
                     <CircleNotchIcon className={cx('loading')} />
                     <span className={cx('separate')}></span>
                     <button className={cx('search-btn')}>
