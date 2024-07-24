3dfa06667d0c0cc6be5adaacdefa6d6d3490b8e7
BaoNhanTran <tranxuandinh181092@gmail.com>
2024-07-24 13:58:29 +0700

    Bài 94. Cấu hình Router/Layout cho dự án AND Bài 95. Dựng khung Layout mặc định

---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+import config from '~/config';
+
+// Layouts
+import { HeaderOnly } from '~/layouts';
+
+// Pages
+import { Home, Following, Profile, Upload, Search, Live, NotFound } from '~/pages';
+
+// Public routes
+const publicRoutes = [
+    { path: config.routes.home, component: Home },
+    { path: config.routes.following, component: Following },
+    { path: config.routes.profile, component: Profile },
+    { path: config.routes.search, component: Search },
+    { path: config.routes.upload, component: Upload, layout: HeaderOnly },
+    { path: config.routes.live, component: Live, layout: null },
+    { path: config.routes.notFound, component: NotFound, layout: null },
+];
+
+// Private routes
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
