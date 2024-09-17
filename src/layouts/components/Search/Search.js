818726241b9e889e85e459166d117a9eb7876d8b
BaoNhanTran <tranxuandinh181092@gmail.com>
2024-09-17 08:28:47 +0700

    Bài 103. Xây dựng Logic phần Header #1

--- src/layouts/components/Search/Search.js
+++ src/layouts/components/Search/Search.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react';
 import HeadlessTippy from '@tippyjs/react/headless';
 import { CircleNotchIcon, CircleXmarkIcon, MagnifyingGlassIcon } from '~/Icons';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
@@ -8,14 +9,40 @@ import styles from './Search.module.scss';
 const cx = classNames.bind(styles);
 
 function Search() {
+    const [searchValue, setSearchValue] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+    const [showResult, setShowResult] = useState(false);
+
+    const inputRef = useRef('');
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResults([1, 2]);
+        }, 0);
+    }, []);
+
+    const handleInput = (e) => {
+        const searchValue = e.target.value;
+        if (!searchValue.startsWith(' ')) {
+            setSearchValue(searchValue);
+        }
+    };
+
+    const handleClear = () => {
+        inputRef.current.focus();
+        setSearchValue('');
+    };
+
     return (
         // Interactive tippy element may not be accessible via keyboard navigation
         // because it is not directly after the reference element in the DOM source order.
         // Using a wrapper <div> tag around the reference element solves this by creating a new parentNode context.
         <div>
             <HeadlessTippy
+                visible={!!searchValue && showResult}
                 interactive
                 offset={[0, 8]}
+                onClickOutside={() => setShowResult(false)}
                 render={(attrs) => (
                     <div className={cx('search-result')} tabIndex="-1" {...attrs}>
                         <PopperWrapper>
@@ -28,9 +55,15 @@ function Search() {
                     </div>
                 )}
             >
-                <form className={cx('search')}>
-                    <input placeholder="Search" />
-                    <button className={cx('clear-btn')}>
+                <div className={cx('search')}>
+                    <input
+                        placeholder="Search"
+                        value={searchValue}
+                        onChange={handleInput}
+                        ref={inputRef}
+                        onFocus={() => setShowResult(true)}
+                    />
+                    <button className={cx('clear-btn')} onClick={handleClear}>
                         <CircleXmarkIcon />
                     </button>
                     <CircleNotchIcon className={cx('loading')} />
@@ -38,7 +71,7 @@ function Search() {
                     <button className={cx('search-btn')}>
                         <MagnifyingGlassIcon />
                     </button>
-                </form>
+                </div>
             </HeadlessTippy>
         </div>
     );
